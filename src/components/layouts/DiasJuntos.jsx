5299eeb131011cf8c33863fbddb68afb0363edd4
Bruno Lima <164899949+euBrunoLima@users.noreply.github.com>
2025-06-26 11:52:07 -0300

    melhorias na pagina

--- src/components/layouts/DiasJuntos.jsx
+++ src/components/layouts/DiasJuntos.jsx
@@ -5,9 +5,9 @@ import anima from '../../styles/animations.module.css'
 function DiasJuntos({ tempoJuntos }) {
 
   return (
-    <div className={`${styles.dias_juntos_container} ${anima.fadeIn}`}>
+    <div className={`${styles.dias_juntos_container} ${anima.fadeIn_2}`}>
       <h1>Eu te amo Há:</h1>
-      <p>{tempoJuntos}</p>
+      <p className={anima.fadeIn_3}>{tempoJuntos}</p>
       <hr />
     </div>
   );
--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,8 +1,23 @@
 import styles from './Footer.module.css'
+import ScrollReveal from 'scrollreveal';
+import { useEffect, useRef } from 'react';
 
 function Footer(){
+
+    const div_ref = useRef(null)
+
+    useEffect(() =>{
+        ScrollReveal().reveal(div_ref.current,{
+            origin: 'bottom',
+            distance: '20px',
+            duration: '1000',
+            delay: 400,
+            easing: 'ease-in-out',
+            reset: false
+        })
+    }, [])
     return(
-        <footer className={styles.footer}>
+        <footer ref={div_ref} className={styles.footer}>
             <span>Desenvolvido por: <a href="https://github.com/euBrunoLima" target='_blanck'>Bruno Lima</a></span>
         </footer>
     )
